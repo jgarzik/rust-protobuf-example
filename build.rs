3ef41016af6c9cde73d1b8823c9c42318dc57f84
Jeff Garzik <jeff@bloq.com>
2022-06-22 01:06:09 -0400

    Refresh to latest protobuf 2.x. Fix warnings.

--- build.rs
+++ build.rs
@@ -1,14 +1,10 @@
 extern crate protoc_rust;
 
-use protoc_rust::Customize;
-
 fn main() {
-	protoc_rust::run(protoc_rust::Args {
-	    out_dir: "src/protos",
-	    input: &["src/protos/example.proto"],
-	    includes: &["src/protos"],
-	    customize: Customize {
-	      ..Default::default()
-	    },
-	}).expect("protoc");
+    protoc_rust::Codegen::new()
+        .out_dir("src/protos")
+        .inputs(&["src/protos/example.proto"])
+        .includes(&["src/protos"])
+        .run()
+        .expect("protoc");
 }
